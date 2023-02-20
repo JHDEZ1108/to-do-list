c60552f7b9dde3acdc1099ada08adb47f14af35b
Josué Hernández <joheca1998@outlook.com>
2023-02-20 14:00:54 -0600

    Actualizando ToDoForm

--- src/Components/TodoForm/index.js
+++ src/Components/TodoForm/index.js
@@ -1,45 +1,91 @@
 import React from 'react';
-import './TodoForm.css';
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  FormControl,
+  TextField,
+  Box,
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const CustomTextField = styled(TextField)({
+  '& label.Mui-focused': {
+    color: '#b0c2f2',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#b0c2f2',
+    },
+    '&:hover fieldset': {
+      borderColor: '#b0c2f2',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#b0c2f2',
+    },
+  },
+});
 
 function TodoForm({ addTodo, setOpenModal }) {
   const [newTodoValue, setNewTodoValue] = React.useState('');
 
-  const onChange = (event) => {
+  const handleChange = (event) => {
     setNewTodoValue(event.target.value);
   };
-  const onCancel = () => {
-    setOpenModal(false);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    addTodo(newTodoValue);
+
+  const handleClose = () => {
     setOpenModal(false);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addTodo(newTodoValue);
+    handleClose();
+  };
+
   return (
-    <form onSubmit={onSubmit}>
-      <label>Escribe tu nuevo TODO</label>
-      <textarea
-        value={newTodoValue}
-        onChange={onChange}
-        placeholder="Cortar la cebolla oara el almuerzo"
-      />
-      <div className="TodoForm-buttonContainer">
-        <button
-          type="button"
-          className="TodoForm-button TodoForm-button--cancel"
-          onClick={onCancel}
-          >
-          Cancelar
-        </button>
-        <button
-          type="submit"
-          className="TodoForm-button TodoForm-button--add"
-        >
-          Añadir
-        </button>
-      </div>
-    </form>
+    <Dialog
+      open={true}
+      onClose={handleClose}
+      fullWidth={true}
+      maxWidth="md"
+    >
+      <DialogTitle sx={{ pt: 4 }}>Añadir To Do</DialogTitle>
+      <DialogContent 
+        sx={{ 
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <FormControl fullWidth>
+          <CustomTextField
+            label="¿Qué debes hacer?"
+            multiline
+            rows={3}
+            variant="outlined"
+            value={newTodoValue}
+            onChange={handleChange}
+            sx={{ mt: 2 }}
+          />
+        </FormControl>
+      </DialogContent>
+      <DialogActions sx={{ justifyContent: 'center', pb: 4 }}>
+        <Box sx={{ mx: 3 }}>
+          <Button onClick={handleClose} color="error" variant="contained" style={{ backgroundColor: '#ff6961' }}>
+            Cancelar
+          </Button>
+        </Box>
+        <Box sx={{ mx: 3 }}>
+          <Button onClick={handleSubmit} color="primary" variant="contained" style={{ backgroundColor: '#b0c2f2' }}>
+            Añadir
+          </Button>
+        </Box>
+      </DialogActions>
+    </Dialog>
   );
 }
 
