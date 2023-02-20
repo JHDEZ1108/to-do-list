62c6807effd2c22f3caf4a2c0f30168feb2e00bb
Josué Hernández <joheca1998@outlook.com>
2023-02-20 15:37:03 -0600

    actualizando ChangeAlert componen

--- src/Components/ChangeAlert/index.js
+++ src/Components/ChangeAlert/index.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { useStorageListener } from './useStorageListener';
-import './ChangeAlert.css';
+import { Dialog, DialogContent, DialogTitle, Button, Box, Typography } from '@mui/material';
 
 function ChangeAlert({ sincronize }) {
   const { show, toggleShow } = useStorageListener(sincronize);
-  
-  if (show) {
-    return (
-      <div className="ChangeAlert-bg">
-        <div className="ChangeAlert-container">
-          <p>Parece que cambiaste tus TODOs en otra pestaña o ventana del navegador.</p>
-          <p>¿Quieres sincronizar tus TODOs?</p>
-          <button
-            className="TodoForm-button TodoForm-button--add"
-            onClick={toggleShow}
-          >
-            Yes!
-          </button>
-        </div>
-      </div>
-    );
-  } else {
-    return null;
-  }
+
+  return (
+    <Dialog open={show} onClose={toggleShow} PaperProps={{ style: { backgroundColor: 'transparent', boxShadow: 'none' } }}>
+      <DialogTitle disableTypography sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Typography variant="h5" sx={{ color: 'white' }}>
+          ¡Atención!
+        </Typography>
+      </DialogTitle>
+      <DialogContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: '24px' }}>
+        <Typography variant="body1" sx={{ color: 'white', marginBottom: '16px', textAlign: 'center' }}>
+          Parece que cambiaste tus ToDo en otra pestaña o ventana del navegador.
+        </Typography>
+        <Typography variant="body1" sx={{ color: 'white', marginBottom: '24px', textAlign: 'center' }}>
+          ¿Quieres sincronizar tus ToDo?
+        </Typography>
+        <Button variant="contained" size="large" onClick={toggleShow} sx={{ borderRadius: '24px' }} style={{ backgroundColor: '#ff6961' }}>
+          Sincronizar
+        </Button>
+      </DialogContent>
+    </Dialog>
+  );
 }
 
 export { ChangeAlert };
+
